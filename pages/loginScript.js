1bc8fd3d42be588c1c94715ce5751d4d52d0e82d
AbdulKhaliq59 <kananuraabdulkhaliq59@gmail.com>
2023-01-18 12:09:13 +0200

    New responsive style added on

--- pages/loginScript.js
+++ pages/loginScript.js
@@ -1,7 +1,6 @@
 function login(e) {
   // e.preventDefault();
   let firstDetail = JSON.parse(localStorage.getItem("userDetails"));
-  // console.log(firstDetail[0].password);
   var email = document.getElementById("email");
   var password = document.getElementById("password");
   if (validatePassword(password.value)) {
@@ -10,7 +9,6 @@ function login(e) {
       email.value === firstDetail[0].email &&
       password.value === firstDetail[0].password
     ) {
-      // alert("You are successfully logged in");
       return (window.location.href = "/pages/adminPanel/index.html");
     } else {
       alert("username or password is invalid");
@@ -19,10 +17,6 @@ function login(e) {
   else{
     alert('Password Should start with capital letter\n and have 8 minimum character');
   }
-
-  // alert("email "+email.value);
-  // alert("userDetails"+userDetails.userDetails.email);
-  // console.log(userDetails.userDetails.email);
 }
 function validatePassword(password) {
   //Regular expression for 8 minimum characters and Start with capital letter
